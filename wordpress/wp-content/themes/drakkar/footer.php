1a922d67782e40364474b94e4616ca3d28852682
olavostauros <olavostauros@outlook.com>
2025-07-16 12:16:32 -0300

    updated footer.php

--- wordpress/wp-content/themes/drakkar/footer.php
+++ wordpress/wp-content/themes/drakkar/footer.php
@@ -1,15 +1,11 @@
     </div><!-- #page -->
 
-    <footer id="colophon" class="site-footer">
-    	<div class="footer-container">
-    		<div class="site-info">
-    			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Todos os direitos reservados.</p>
-    		</div><!-- .site-info -->
-    	</div>
-    </footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Todos os direitos reservados.</p>
+	</div>
+</footer>
 
-    <?php wp_footer(); ?>
-
-    </body>
-
-    </html>
+<?php wp_footer(); ?>
+</body>
+</html>
